ef7d28d16283e5cb08c943e8f0798916d95bf8a1
chris <c.giacofei@gmail.com>
2016-08-22 11:46:45 -0400

    refactor data parsing code

--- parse_data.py
+++ parse_data.py
@@ -16,36 +16,40 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 from plotting import StackedArea, LinePlot
 
+os.environ['HTTP_PROXY'] = "http://proxyarray.mazdausa.com:80"
+os.environ['HTTPS_PROXY'] = "http://proxyarray.mazdausa.com:80"
+
 # Directories
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 HTML_ROOT = os.path.join(THIS_DIR, 'html')
-LOCATION_DIR = 'location'
-ABS_LOCATION_DIR = os.path.join(HTML_ROOT, 'location')
+LOC_PATH = 'location'
+ABS_LOC_PATH = os.path.join(HTML_ROOT, 'location')
 
 SLUG = UniqueSlugify(to_lower=True)
 
 SO4CL_RATIO = {
-	0: 'Too Malty',
-	0.4: 'Very Malty',
-	.6: 'Malty',
-	.8: 'Balanced',
-	1.5: 'Little Bitter',
-	2.01: 'More Bitter',
-	4.01: 'Extra Bitter',
-	6.01: 'Quite Bitter',
-	8.01: 'Very Bitter',
-	9.01: 'Too Bitter'
+    0: 'Too Malty',
+    0.4: 'Very Malty',
+    .6: 'Malty',
+    .8: 'Balanced',
+    1.5: 'Little Bitter',
+    2.01: 'More Bitter',
+    4.01: 'Extra Bitter',
+    6.01: 'Quite Bitter',
+    8.01: 'Very Bitter',
+    9.01: 'Too Bitter'
 }
 
-def trim(path):
-	im = Image.open(path)
 
-	bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
-	diff = ImageChops.difference(im, bg)
-	diff = ImageChops.add(diff, diff, 2.0, -100)
-	bbox = diff.getbbox()
-	if bbox:
-		im.save(path)
+def trim(path):
+    im = Image.open(path)
+
+    bg = Image.new(im.mode, im.size, im.getpixel((0, 0)))
+    diff = ImageChops.difference(im, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    if bbox:
+        im.save(path)
 
 
 def make_html_doc(template, content):
@@ -55,120 +59,205 @@ def make_html_doc(template, content):
     return j2_env.get_template(template).render(content)
 
 
+def add_columns(df):
+    """ Add calculated columns to the dataframe
+
+        Accepts test result dataframe and return the newly modified dataframe.
+
+    """
+    # Rename columns from google sheets
+    df = df.rename(columns={
+        'Sample ID': 'sample_id',
+        'Sample Date': 'sample_date',
+        'Sample Location': 'sample_location',
+        'Total Hardness': 'total_hardness',
+        'Calcium Hardness': 'ca_hardness',
+        'Total Alkalinity': 'total_alkalinity',
+        'Sulfate': 'so4',
+        'Chlorine': 'cl',
+    })
+
+    # Everything is loaded as strings, need to convert to numeric
+    df['total_hardness'] = df['total_hardness'].apply(pd.to_numeric, args=('coerce',))
+    df['ca_hardness'] = df['ca_hardness'].apply(pd.to_numeric, args=('coerce',))
+    df['total_alkalinity'] = df['total_alkalinity'].apply(pd.to_numeric, args=('coerce',))
+    df['so4'] = df['so4'].apply(pd.to_numeric, args=('coerce',))
+    df['cl'] = df['cl'].apply(pd.to_numeric, args=('coerce',))
+
+    # Add calculated columns
+    df['mg_hardness'] = df['total_hardness'] - df['ca_hardness']
+    df['res_alkalinity'] = df['total_alkalinity'] - (df['ca_hardness'] / 3.5 + df['mg_hardness'] / 7)
+    df['ca2'] = df['ca_hardness'] * 0.4
+    df['mg2'] = df['mg_hardness'] * 0.25
+    df['hco3'] = df['total_alkalinity'] * 1.22
+    df['so4_cl_ratio'] = df['so4'] / df['cl']
+
+    # Add descriptor from SO4 / Cl Ratio Lookup
+    set_ratio = [min(SO4CL_RATIO.keys(), key=lambda x: abs(x - r)) for r in df['so4_cl_ratio']]
+    ratios = [SO4CL_RATIO[value] for value in set_ratio]
+
+    df['balance'] = ratios
+    df['sample_date'] = to_datetime(df['sample_date'], format='%m/%d/%Y')
+    df = df.sort_values(by='sample_date')
+
+    return df
+
+
+def connect_sheets(key, sheet_id, sheet_tab):
+    """ Connect to a google spreadsheet using gspread
+
+        Parameters:
+          key: json key file from google developer console
+          sheet_id: ID of the sheet found in the sheet URL
+          sheet_tab: name of the tab within the worksheet to pull
+
+        Return: gspread worksheet object
+    """
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(key, scope)
+    gc = gspread.authorize(credentials)
+    wks = gc.open_by_key(sheet_id).worksheet(sheet_tab)
+
+    return wks
+
+def build_source_summaries(df, locations):
+    source_pages = []
+
+    for location in locations:
+        page_dict = {'caption': location}
+        page_dict['slug'] = SLUG(location)
+        page_dict['src'] = os.path.join(
+            ABS_LOC_PATH, page_dict['slug'] + '.html'
+        )
+        page_dict['url'] = LOC_PATH + '/' + page_dict['slug'] + '.html'
+
+        filtered = df[df['sample_location'] == location]
+
+        ## Hardness over time (Total, Ca, Mg)
+        x_data = filtered['sample_date']
+        series = ['ca_hardness', 'mg_hardness', 'total_hardness']
+        y_data = [filtered[col] for col in series]
+        ids = filtered['sample_id']
+
+        my_plot = LinePlot(
+            x_data,
+            y_data,
+            series,
+            ids,
+            page_dict['slug'] + '-hardness'
+        )
+
+        png, html = my_plot.plot()
+
+        # Relative path for source locatin pages
+        page_dict['hardness_png'] = os.path.join('..', 'img', png)
+
+        trim(os.path.join('html', 'img', page_dict['hardness_png']))
+
+        ## Alkalinity over time (Total, Residual)
+        series = ['total_alkalinity', 'res_alkalinity']
+        y_data = [filtered[col] for col in series]
+        ids = filtered['sample_id']
+
+        my_plot = LinePlot(
+            x_data,
+            y_data,
+            series,
+            ids,
+            page_dict['slug'] + '-alkalinity'
+        )
+
+        png, html = my_plot.plot()
+
+        # Relative path for source locatin pages
+        page_dict['alkalinity_png'] = os.path.join('..', 'img', png)
+
+        trim(os.path.join('html', 'img', page_dict['alkalinity_png']))
+
+        ## Ion Concentrations over time (Cl-, SO4-, Ca2+, Mg2+, HCO3-)
+        ion_list = [
+            ('cl', 'Chlorine'),
+            ('so4', 'Sulfate'),
+            ('ca2', 'Calcium'),
+            ('mg2', 'Magnesium'),
+            ('hco3', 'Bicarbonate'),
+        ]
+
+        page_dict['ion_png'] = []
+        page_dict['ion_html'] = []
+
+        for ion in ion_list:
+            my_plot = LinePlot(
+                x_data,
+                [filtered[ion[0]]],
+                [ion[1]],
+                ids,
+                page_dict['slug'] + '-' + ion[0]
+            )
+
+            png, html = my_plot.plot(legend=False)
+
+            trim(os.path.join('html', 'img', png))
+
+            png = os.path.join('..', 'img', png)
+            page_dict['ion_png'].append([png, ion[1]])
+            page_dict['ion_html'].append(html)
+
+        ## SO4/Cl Ratio over time
+
+        ## pH over time
+
+        source_pages.append(page_dict)
+
+    return source_pages
+
+
 if __name__ == '__main__':
-	scope = ['https://spreadsheets.google.com/feeds']
-	credentials = ServiceAccountCredentials.from_json_keyfile_name('client_id.json', scope)
-	gc = gspread.authorize(credentials)
-	wks = gc.open_by_key("1Z1XF9nabneWBDbFwaovI_n9YcazeNQq4hon1wsIxrus").worksheet('Data')
-	df = DataFrame(wks.get_all_records())
+    # Set HTML template files
+    loc_tmpl = os.path.join('templates', 'bootstrap_summary_sheet.html')
+    index_tmpl = os.path.join('templates', 'bootstrap_base.html')
+    report_tmpl = os.path.join('templates', 'report.html')
 
-	loc_tmpl = os.path.join('templates', 'bootstrap_summary_sheet.html')
-	index_tmpl = os.path.join('templates', 'bootstrap_base.html')
+    #~ # Connect to Google spreadsheet
+    #~ worksheet = connect_sheets(
+        #~ 'client_id.json',
+        #~ '1Z1XF9nabneWBDbFwaovI_n9YcazeNQq4hon1wsIxrus',
+        #~ 'Data'
+    #~ )
 
-	# Everything is loaded as strings, need to convert to numeric
-	df['Total Hardness'] = df['Total Hardness'].apply(pd.to_numeric, args=('coerce',))
-	df['Calcium Hardness'] = df['Calcium Hardness'].apply(pd.to_numeric, args=('coerce',))
-	df['Total Alkalinity'] = df['Total Alkalinity'].apply(pd.to_numeric, args=('coerce',))
-	df['Sulfate'] = df['Sulfate'].apply(pd.to_numeric, args=('coerce',))
-	df['Chlorine'] = df['Chlorine'].apply(pd.to_numeric, args=('coerce',))
+    #~ # Dump spreadsheet data into dataframe and add calculated columns
+    #~ df = DataFrame(worksheet.get_all_records())
+    df = read_csv('Data.csv')
+    df = add_columns(df)
 
-	# Add calculated columns
-	df['Magnesium Hardness'] = df['Total Hardness'] - df['Calcium Hardness']
-	df['Residual Alkalinity'] = df['Total Alkalinity'] - (df['Calcium Hardness']/3.5 + df['Magnesium Hardness']/7)
-	df['ca2'] = df['Calcium Hardness'] * 0.4
-	df['mg2'] = df['Magnesium Hardness'] * 0.25
-	df['hco3'] = df['Total Alkalinity'] * 1.22
-	df['so4_cl_ratio'] = df['Sulfate'] / df['Chlorine']
+    # List of water sources
+    locations = df['sample_location'].unique()
 
-	# Add descriptor from SO4 / Cl Ratio Lookup
-	set_ratio = [min(SO4CL_RATIO.keys(), key=lambda x: abs(x - r)) for r in df['so4_cl_ratio']]
-	ratios = [SO4CL_RATIO[value] for value in set_ratio]
-	
-	df['balance'] = ratios
-	df['Sample Date'] = to_datetime(df['Sample Date'], format='%Y-%m-%d')
-	df = df.sort_values(by='Sample Date')
+    source_pages = build_source_summaries(df, locations)
 
-	locations = df['Sample Location'].unique()
+    # Convet dataframe to list of dicts for individual records
+    report_pages = df.to_dict('records')
 
-	pages = []
+    ## Write location source pages
+    # source_page keys:
+    #   caption            Page title: Used for page headings
+    #   slug               Page slug: Not used
+    #   src                Source file path: Used for file creation
+    #   url                URL relative to index: Used for page links
+    #   hardness_png       Hardness plot filename: Used for image links
+    #   hardness_html      Hardness plot HTML from mpld3: Not used
+    #   ion_png            List of Ion plot filenames: Used for image links
+    #   ion_html           Not used
 
-	# Headings:
-	#  total_hardness, ca_hardness, mg_hardness
-	#  Total Alkalinity, res_alkalinity
-	#  cl, so4, ca2, mg2, hco3                            Ion Concentrations
-	#  so4_cl                                             SO4/Cl ratio
-	#  ph
-	for location in locations:
-		page_dict = {'caption': location}
-		page_dict['slug'] = SLUG(location)
-		page_dict['src'] = os.path.join(ABS_LOCATION_DIR, page_dict['slug'] + '.html')
-		page_dict['url'] = LOCATION_DIR + '/' + page_dict['slug'] + '.html'
+    for page in source_pages:
+        with open(page['src'], 'w') as html_file:
+            html_file.write(make_html_doc(loc_tmpl, {
+                'thispage': page,
+                'pages': source_pages
+            }))
 
-		filtered = df[df['Sample Location'] == location]
-
-		## Hardness over time (Total, Ca, Mg)
-		my_plot = LinePlot(
-			filtered['Sample Date'],
-			[filtered['Calcium Hardness'], filtered['Magnesium Hardness']],
-			['Ca Hardness', 'Mg Hardness'],
-			filtered['Sample ID'],
-			page_dict['slug'] + '-hardness'
-		)
-
-		page_dict['hardness_plt_png'], page_dict['hardness_plt_html'] = my_plot.plot()
-		page_dict['hardness_plt_png'] = os.path.join(
-			'..',
-			'img',
-			page_dict['hardness_plt_png']
-		)
-		trim(os.path.join('html', 'img', page_dict['hardness_plt_png']))
-		## Alkalinity over time (Total, Residual)
-
-		## Ion Concentrations over time (Cl-, SO4-, Ca2+, Mg2+, HCO3-)
-		ion_list = [
-			('Chlorine', 'Cl-'),
-			('Sulfate', 'SO4-'),
-			('ca2', 'Ca2+'),
-			('mg2', 'Mg2+'),
-			('hco3', 'HCO3-'),
-		]
-		dates = filtered['Sample Date']
-		ids = filtered['Sample ID']
-
-		page_dict['ion_png'] = []
-		page_dict['ion_html'] = []
-
-		for ion in ion_list:
-			my_plot = LinePlot(
-				dates,
-				[filtered[ion[0]]],
-				[ion[1]],
-				ids,
-				page_dict['slug'] + '-' + ion[0]
-			)
-
-			png, html = my_plot.plot()
-
-			trim(os.path.join('html', 'img', png))
-
-			png = os.path.join('..', 'img', png)
-
-			page_dict['ion_png'].append([png, ion[1]])
-			page_dict['ion_html'].append(html)
-
-		## SO4/Cl Ratio over time
-
-		## pH over time
-
-		pages.append(page_dict)
-
-	## Write Location Pages
-	for page in pages:
-		if page['slug'] != 'Home':
-			with open(page['src'], 'w') as html_file:
-				html_file.write(make_html_doc(loc_tmpl, {'thispage': page, 'pages': pages}))
-
-	## Make main index
-	with open(HTML_ROOT + '/index.html', 'w') as html_file:
-		html_file.write(make_html_doc(index_tmpl, {'pages': pages}))
-
+    ## Make main index
+    with open(HTML_ROOT + '/index.html', 'w') as html_file:
+        html_file.write(make_html_doc(index_tmpl, {
+            'source_pages': source_pages
+        }))
