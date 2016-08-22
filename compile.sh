36f565871580cb4dac5672820176e899a346684b
chris <c.giacofei@gmail.com>
2016-08-22 11:54:27 -0400

    Fix copying static files to html directory.

--- compile.sh
+++ compile.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 mkdir -p html/location html/result html/img
-cp -r css html/css
-cp -r js html/js
+cp -r css/* html/stylesheets
+cp -r js/* html/js
+cp -r fonts/* html/fonts
 
 python parse_data.py
